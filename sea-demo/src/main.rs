52f2c36e568527ad17892fa850c445f514d613d9
seaung <seaung.c0m7de@protonmail.com>
2024-03-30 21:55:50 +0800

    添加数据库迁移代码

--- sea-demo/src/main.rs
+++ sea-demo/src/main.rs
@@ -1,11 +1,39 @@
 use futures::executor::block_on;
-use sea_orm::{Database, DbErr};
+use sea_orm::{ConnectionTrait, Database, DbErr, DbBackend, Statement};
 
 const DATABASE_URI: &str = "mysql://root:7oty\\@7#667989@localhost:3306";
 const DB_NAME: &str = "bseaorm";
 
 async fn run() -> Result<(), DbErr> {
     let db = Database::connect(DATABASE_URI).await?;
+
+    let db = &match db.get_database_backend() {
+        DbBackend::MySql => {
+            db.execute(
+                Statement::from_string(db.get_database_backend(), 
+                format!("CREATE DATABASE IF NOT EXISTS `{}`;", DB_NAME),
+            )).await?;
+            let url = format!("{}/{}", DATABASE_URI, DB_NAME);
+            Database::connect(&url).await?
+        }
+
+        DbBackend::Postgres => {
+            db.execute(
+                Statement::from_string(db.get_database_backend(), 
+                format!("DROP DATABASE IF EXISTS \"{}\";", DB_NAME),
+            )).await?;
+
+            db.execute(Statement::from_string(
+               db.get_database_backend(),
+               format!("CREATE DATABASE \"{}\";", DB_NAME),
+           )).await?;
+
+            let url = format!("{}/{}", DATABASE_URI, DB_NAME);
+            Database::connect(&url).await?
+        }
+
+        DbBackend::Sqlite => db
+    };
     Ok(())
 }
 
